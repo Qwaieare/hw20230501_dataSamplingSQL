1db33446cde11ed0fc77ba9f242ad2d2339e7aa3
Qwaieare <gb-grand@mail.ru>
2023-05-02 00:44:09 +0300

    initial commit for hw20230501_dataSamplingSQL

---
+++ src/main/java/skypro/datasamplingsql/Hw20230501DataSamplingSqlApplication.java
@@ -0,0 +1,13 @@
+package skypro.datasamplingsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw20230501DataSamplingSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw20230501DataSamplingSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/datasamplingsql/Hw20230501DataSamplingSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.datasamplingsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw20230501DataSamplingSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
